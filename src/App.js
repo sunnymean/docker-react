827e37206959873c086788a2e9e2d60c1ff7f8a5
sunnymean <sryu@hist.co.kr>
2023-03-31 10:34:52 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Learn React
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noop ener noreferrer"
         >
-          Learn React
+          I was changed! at local
         </a>
       </header>
     </div>
